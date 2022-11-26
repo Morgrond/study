5cea00b3db122801ceebdccd2245498e74e5f505
Morgrond <115943821+Morgrond@users.noreply.github.com>
2022-11-26 15:25:43 +0300

    Add files via upload
    Updated table with 3 rows

--- 3.6.2.py
+++ 3.6.2.py
@@ -6,3 +6,53 @@ cur.execute('''CREATE TABLE IF NOT EXISTS types (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 type VARCHAR(100) NOT NULL); ''')
 conn.commit()
+
+
+def insert_type(String_type):
+    try:
+        sqlite_connection = sqlite3.connect('My_cats.db')
+        cursor = sqlite_connection.cursor()
+        print('Подключен к SQLite')
+
+        sqlite_insert_query = """INSERT INTO types (id, type) VALUES (?, ?);"""
+
+        cursor.executemany(sqlite_insert_query, String_type)
+        sqlite_connection.commit()
+        print('Записи успешно вставлены в таблицу types', cursor.rowcount)
+        sqlite_connection.commit()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print('Ошибка при работе с SQLite', error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print('Соединение с SQLite закрыто')
+
+records_to_insert = [(1, 'Абиссинская кошка'),
+                     (2, 'Австралийский мист'),
+                     ('3', 'Американская жесткошерстная')]
+
+insert_type(records_to_insert)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
